33561bcfe44ede0747177df4c1cbbc1081e432c3
lelbil <billelattouchi@yahoo.fr>
2018-09-21 10:39:20 +0200

    basic non-functional UI

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Add } from 'material-ui-icons'
+import Drawer from 'material-ui/Drawer'
+import TextField from 'material-ui/TextField'
+import Checkbox from 'material-ui/Checkbox'
+import Slider from 'material-ui/Slider'
+import DatePicker from 'hotel-datepicker'
+
+import Carte from './Components/Carte'
+
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            logged: false,
+            filtersOpen: true,
+        }
+    }
+
+    componentDidMount() {
+        //new DatePicker(document.getElementById('input-id'), {});
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <MuiThemeProvider>
+              <Toolbar style={styles.toolbar} id={"toolbar"}>
+                  <ToolbarGroup style={{ display: 'flex', width: '100%' }} id={"toolbarGroup"}>
+                      <ToolbarTitle text="CompaLoca - Slogan"/>
+                      <ToolbarSeparator/>
+                      <React.Fragment>
+                          <RaisedButton style={{ marginLeft: 'auto' }} primary={true}>Connexion</RaisedButton>
+                          <ToolbarSeparator/>
+                          <RaisedButton primary={true}>Inscription</RaisedButton>
+                      </React.Fragment>
+                  </ToolbarGroup>
+              </Toolbar>
+              <Drawer containerStyle={{overflow: "visible"}} open={this.state.filtersOpen}>
+                  <TextField
+                      floatingLabelText="Lieu"
+                  />
+                  <Slider/>
+                  <Slider/>
+                  <TextField floatingLabelText="dates" id="input-id" type="text"/>
+                  <Checkbox
+                      label="Appartement"
+                      checked={true}
+                      onCheck={() => {console.log('CI')}}
+                  />
+              </Drawer>
+              <div>
+                 <Carte/>
+              </div>
+          </MuiThemeProvider>
       </div>
     );
   }
+}
+
+const styles = {
+    toolbar: {
+        backgroundColor: "rgba(70, 153, 135, 0.7)",
+        borderBottom: "solid 2px",
+    },
 }
 
 export default App;
