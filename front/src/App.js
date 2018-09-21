4f3bd48adebaa2c8e6b071da3bf5c72429cc7838
lelbil <billelattouchi@yahoo.fr>
2018-09-21 14:14:42 +0200

    get rid of filters

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,7 @@ import React, { Component } from 'react';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Add } from 'material-ui-icons'
-import Drawer from 'material-ui/Drawer'
 import TextField from 'material-ui/TextField'
-import Checkbox from 'material-ui/Checkbox'
-import Slider from 'material-ui/Slider'
-import DatePicker from 'hotel-datepicker'
 import _ from 'lodash'
 
 import Carte from './Components/Carte'
@@ -19,49 +14,21 @@ class App extends Component {
         super(props)
         this.state = {
             logged: false,
-            filtersOpen: true,
             lieu: '',
             latitude: 48.8574848,
             longitude: 2.3168774,
-            logements: [
-                {
-                    "id": "1",
-                    "intitule": "Appartement 2 pièces",
-                    "loyer": "500",
-                    "latitude": "48.8425",
-                    "longitude": "2.33259",
-                    "ville": "Paris"
-                },
-                {
-                    "id": "2",
-                    "intitule": "Petit Studio ",
-                    "loyer": "600",
-                    "latitude": "48.8352",
-                    "longitude": "2.35822",
-                    "ville": "Paris"
-                },
-                {
-                    "id": "3",
-                    "intitule": "Chambre privée dans un appartement",
-                    "loyer": "434",
-                    "latitude": "48.8918",
-                    "longitude": "2.36034",
-                    "ville": "Paris"
-                },
-                {
-                    "id": "4",
-                    "intitule": "Place de Parking",
-                    "loyer": "121",
-                    "latitude": "48.8343",
-                    "longitude": "2.25532",
-                    "ville": "PARIS"
-                }
-            ],
+            logements: [],
         }
     }
 
     componentDidMount() {
-        new DatePicker(document.getElementById('input-id'), {});
+        fetch('http://localhost/api/logements.php?ville=Paris')
+            .then(res => res.json())
+            .then(data => {
+                const { logements } = data
+                this.setState({ logements })
+            })
+
     }
 
     changeLieu = lieu => {
@@ -98,8 +65,22 @@ class App extends Component {
               <div>
               <Toolbar style={styles.toolbar} id={"toolbar"}>
                   <ToolbarGroup style={{ display: 'flex', width: '100%' }} id={"toolbarGroup"}>
-                      <ToolbarTitle text="CompaLoca - Slogan"/>
+                      <ToolbarTitle text="CompaLoca - Comparateur de locations"/>
                       <ToolbarSeparator/>
+                      <TextField
+                          hintText="Lieu"
+                          onChange={({target}) => {
+                              this.setState({ lieu: target.value})
+                              clearTimeout(this.typingTimeout)
+                              this.typingTimeout = setTimeout(() => {
+                                  this.changeLieu(target.value)
+                              }, 1000)
+                          }}
+                          style={{ margin: 'auto', width: '40%' }}
+                          hintStyle={{ color: '#DDD'}}
+                          value={this.state.lieu}
+                          autoFocus={true}
+                      />
                       <React.Fragment>
                           <RaisedButton style={{ marginLeft: 'auto' }} primary={true}>Connexion</RaisedButton>
                           <ToolbarSeparator/>
@@ -107,28 +88,6 @@ class App extends Component {
                       </React.Fragment>
                   </ToolbarGroup>
                   </Toolbar>
-                  <Drawer containerStyle={{overflow: "visible"}} open={this.state.filtersOpen}>
-                      <TextField
-                          floatingLabelText="Lieu"
-                          onChange={({target}) => {
-                              this.setState({ lieu: target.value})
-                              clearTimeout(this.typingTimeout)
-                              this.typingTimeout = setTimeout(() => {
-                                      this.changeLieu(target.value)
-                                  }, 1000)
-
-                          }}
-                          value={this.state.lieu}
-                      />
-                      <Slider/>
-                      <Slider/>
-                      <TextField floatingLabelText="dates" id="input-id" type="text"/>
-                      <Checkbox
-                          label="Appartement"
-                          checked={true}
-                          onCheck={() => {console.log('CI')}}
-                      />
-                  </Drawer>
                   <Carte logements={this.state.logements} latitude={this.state.latitude} longitude={this.state.longitude}/>
               </div>
           </MuiThemeProvider>
