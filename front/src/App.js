0e4e5fbf0cda5b3d7bb15a7d183c24d0e43ae56b
lelbil <billelattouchi@yahoo.fr>
2018-09-21 11:39:21 +0200

    funcitonal MVP without filters apart from city search

--- front/src/App.js
+++ front/src/App.js
@@ -8,6 +8,7 @@ import TextField from 'material-ui/TextField'
 import Checkbox from 'material-ui/Checkbox'
 import Slider from 'material-ui/Slider'
 import DatePicker from 'hotel-datepicker'
+import _ from 'lodash'
 
 import Carte from './Components/Carte'
 
@@ -19,17 +20,82 @@ class App extends Component {
         this.state = {
             logged: false,
             filtersOpen: true,
+            lieu: '',
+            latitude: 48.8574848,
+            longitude: 2.3168774,
+            logements: [
+                {
+                    "id": "1",
+                    "intitule": "Appartement 2 pièces",
+                    "loyer": "500",
+                    "latitude": "48.8425",
+                    "longitude": "2.33259",
+                    "ville": "Paris"
+                },
+                {
+                    "id": "2",
+                    "intitule": "Petit Studio ",
+                    "loyer": "600",
+                    "latitude": "48.8352",
+                    "longitude": "2.35822",
+                    "ville": "Paris"
+                },
+                {
+                    "id": "3",
+                    "intitule": "Chambre privée dans un appartement",
+                    "loyer": "434",
+                    "latitude": "48.8918",
+                    "longitude": "2.36034",
+                    "ville": "Paris"
+                },
+                {
+                    "id": "4",
+                    "intitule": "Place de Parking",
+                    "loyer": "121",
+                    "latitude": "48.8343",
+                    "longitude": "2.25532",
+                    "ville": "PARIS"
+                }
+            ],
         }
     }
 
     componentDidMount() {
-        //new DatePicker(document.getElementById('input-id'), {});
+        new DatePicker(document.getElementById('input-id'), {});
+    }
+
+    changeLieu = lieu => {
+        if (!lieu) return
+
+        fetch(`http://localhost/api/logements.php?ville=${lieu}`)
+            .then(res => res.json())
+            .then(data => {
+                const { logements } = data
+                this.setState({ logements })
+            })
+
+        fetch(`http://photon.komoot.de/api/?q=${lieu}&lang=fr&limit=1`)
+            .then(res => res.json())
+            .then(data => {
+                const coordinates =_.get(data, 'features[0].geometry.coordinates')
+                if (!coordinates) return
+
+                const [longitude, latitude] = coordinates
+
+                this.setState({
+                    latitude,
+                    longitude,
+                })
+            })
+
+        this.setState({ lieu })
     }
 
   render() {
     return (
       <div className="App">
           <MuiThemeProvider>
+              <div>
               <Toolbar style={styles.toolbar} id={"toolbar"}>
                   <ToolbarGroup style={{ display: 'flex', width: '100%' }} id={"toolbarGroup"}>
                       <ToolbarTitle text="CompaLoca - Slogan"/>
@@ -40,22 +106,30 @@ class App extends Component {
                           <RaisedButton primary={true}>Inscription</RaisedButton>
                       </React.Fragment>
                   </ToolbarGroup>
-              </Toolbar>
-              <Drawer containerStyle={{overflow: "visible"}} open={this.state.filtersOpen}>
-                  <TextField
-                      floatingLabelText="Lieu"
-                  />
-                  <Slider/>
-                  <Slider/>
-                  <TextField floatingLabelText="dates" id="input-id" type="text"/>
-                  <Checkbox
-                      label="Appartement"
-                      checked={true}
-                      onCheck={() => {console.log('CI')}}
-                  />
-              </Drawer>
-              <div>
-                 <Carte/>
+                  </Toolbar>
+                  <Drawer containerStyle={{overflow: "visible"}} open={this.state.filtersOpen}>
+                      <TextField
+                          floatingLabelText="Lieu"
+                          onChange={({target}) => {
+                              this.setState({ lieu: target.value})
+                              clearTimeout(this.typingTimeout)
+                              this.typingTimeout = setTimeout(() => {
+                                      this.changeLieu(target.value)
+                                  }, 1000)
+
+                          }}
+                          value={this.state.lieu}
+                      />
+                      <Slider/>
+                      <Slider/>
+                      <TextField floatingLabelText="dates" id="input-id" type="text"/>
+                      <Checkbox
+                          label="Appartement"
+                          checked={true}
+                          onCheck={() => {console.log('CI')}}
+                      />
+                  </Drawer>
+                  <Carte logements={this.state.logements} latitude={this.state.latitude} longitude={this.state.longitude}/>
               </div>
           </MuiThemeProvider>
       </div>
--- front/src/Components/Carte.js
+++ front/src/Components/Carte.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
 export default class Carte extends Component {
     state = {
-        lat: 51.505,
-        lng: -0.09,
         zoom: 13,
     }
 
     render() {
-        const position = [this.state.lat, this.state.lng]
+        const position = [this.props.latitude, this.props.longitude]
         return (
             <Map center={position} zoom={this.state.zoom}>
                 <TileLayer
                     attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {this.props.logements.map(logement => {
+                    return (
+                        <Marker key={logement.id} position={[logement.latitude, logement.longitude]}>
+                            <Popup>
+                                <h4>{logement.intitule}</h4>
+                                <h5><strong>{logement.loyer}</strong>€/mois</h5>
+                            </Popup>
+                        </Marker>
+                    )
+                })}
             </Map>
         )
     }
